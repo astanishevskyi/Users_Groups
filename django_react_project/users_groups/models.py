f5a04e4b0e5ec04f911749644a88bc4f4fb9d1f4
sandr463 <stanish2000@gmail.com>
2019-04-21 14:04:19 +0300

    Change decription in model Group

--- django_react_project/users_groups/models.py
+++ django_react_project/users_groups/models.py
@@ -12,7 +12,7 @@ class User(models.Model):
 
 class Group(models.Model):
     name = models.CharField('name', max_length=255, null=False, blank=False)
-    description = models.TextField(null=False, blank=False)
+    description = models.CharField('description', null=False, blank=False, max_length=255)
 
     def __str__(self):
         return self.name
