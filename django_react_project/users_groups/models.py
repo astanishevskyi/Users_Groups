39ff6c049e64d56c5d3adb18fc88ca2ae9203c88
sandr463 <stanish2000@gmail.com>
2019-04-21 07:51:15 +0300

    Change models

--- django_react_project/users_groups/models.py
+++ django_react_project/users_groups/models.py
@@ -5,7 +5,6 @@ class User(models.Model):
     username = models.CharField('username', max_length=255)
     created = models.DateTimeField('Created at', auto_now_add=True)
     group = models.ForeignKey('Group', on_delete=models.CASCADE,)
-    # actions = models.  Edit and Delete
 
     def __str__(self):
         return self.username
@@ -14,7 +13,6 @@ class User(models.Model):
 class Group(models.Model):
     name = models.CharField('name', max_length=255, null=False, blank=False)
     description = models.TextField(null=False, blank=False)
-    # actions Edit and Delete
 
     def __str__(self):
         return self.name
